488e01474a6a2cead947bb843ee2dce5858599fb
Taimin <taiminyang@gmail.com>
2017-10-19 18:18:44 +0200

    Added save fcalc and focs as json file and put some functions into the mainwindow class. It is wired that a window that can read files but for this small thing it may just work. Next thing will be read and display files on a new window

--- Fcalc_Fobs.py
+++ Fcalc_Fobs.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 14:00:27 2017
+
+@author: Taimin Yang
+"""
+
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QInputDialog, QLineEdit, QFileDialog, QCheckBox, QButtonGroup, QMessageBox, QComboBox 
 from PyQt5.QtGui import QIcon, QDoubleValidator
 from PyQt5 import QtCore
 import sys, traceback, os
+import json
 import cctbx
 import numpy as np
 import pandas as pd
@@ -20,6 +28,7 @@ class Main_Window(QMainWindow):
 		self.pdb_name = ""
 		self.fcf_name = ""
 		self.cif_name = ""
+		self.saved_Data = {}
 		self.resolution = 1.8
 		self.scaling_factor = 1
 		self.scatfact_table = 'electron'
@@ -38,9 +47,8 @@ class Main_Window(QMainWindow):
 	def load_mtz(self):
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
-		self.mtz_name, _ = QFileDialog.getOpenFileName(self,"Chooose an MTZ file", "","All Files (*);;MTZ Files (*.mtz)", options=options)
+		self.mtz_name, _ = QFileDialog.getOpenFileName(self,"Chooose an MTZ file", "","MTZ Files (*.mtz);;All Files (*)", options=options)
 		self.textbox1.setText(self.mtz_name)
-
 	def mtz_text_editor_enter(self):
 		tmp = self.textbox1.text()
 		if os.path.isfile(tmp):
@@ -49,14 +57,28 @@ class Main_Window(QMainWindow):
 			msgBox.setText("Successfully found MTZ file.")
 			msgBox.setWindowTitle("MTZ File")
 			msgBox.exec_()
+		else:
+			msgBox = QMessageBox()
+			msgBox.setText("File does not exist, please enter another name")
+			msgBox.setWindowTitle("File")
+			msgBox.exec_()
+	def read_mtz(self,f):
+		#read an mtz file
+		return any_reflection_file(str(f))
 	def show_mtz(self):
-		pass
+		#try:
+		self.F_Window = File_Window(str(self.mtz_name))
+		#except:
+		#	msgBox = QMessageBox()
+		#	msgBox.setText("Failed to show MTZ file. \nPlease check if your path is right.")
+		#	msgBox.setWindowTitle("MTZ File")
+		#	msgBox.exec_()
 	
 	#Operations about PDB file
 	def load_pdb(self):
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
-		self.pdb_name, _ = QFileDialog.getOpenFileName(self,"Choose a PDB file", "","All Files (*);;PDB Files (*.pdb)", options=options)
+		self.pdb_name, _ = QFileDialog.getOpenFileName(self,"Choose a PDB file", "","PDB Files (*.pdb);;All Files (*)", options=options)
 		self.textbox2.setText(self.pdb_name)
 	def pdb_text_editor_enter(self):
 		tmp = self.textbox2.text()
@@ -65,6 +87,23 @@ class Main_Window(QMainWindow):
 			msgBox = QMessageBox()
 			msgBox.setText("Successfully found PDB file.")
 			msgBox.setWindowTitle("PDB File")
+		else:
+			msgBox = QMessageBox()
+			msgBox.setText("File does not exist, please enter another name")
+			msgBox.setWindowTitle("File")
+			msgBox.exec_()
+	def read_pdb(self,f):
+		#read a pdb file
+		try:
+			if isinstance(f,str):
+				structures=pdb.input(file_name=f,raise_sorry_if_format_error=True).xray_structure_simple()
+			else:
+				raise TypeError,'read_pdb: Cannot deal is type {}'.format(type(f))
+		except libtbx.utils.Sorry as e:
+			print e
+			print "Error parsing pdb file, check if the data tag does not contain any spaces."
+			exit()
+		return structures
 	def show_pdb(self):
 		pass
 		
@@ -72,7 +111,7 @@ class Main_Window(QMainWindow):
 	def load_fcf(self):
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
-		self.fcf_name, _ = QFileDialog.getOpenFileName(self,"Choose an FCF file", "","All Files (*);;FCF Files (*.fcf)", options=options)
+		self.fcf_name, _ = QFileDialog.getOpenFileName(self,"Choose an FCF file", "","FCF Files (*.fcf);;All Files (*)", options=options)
 		self.textbox3.setText(self.fcf_name)
 	def fcf_text_editor_enter(self):
 		tmp = self.textbox3.text()
@@ -81,14 +120,22 @@ class Main_Window(QMainWindow):
 			msgBox = QMessageBox()
 			msgBox.setText("Successfully found FCF file.")
 			msgBox.setWindowTitle("FCF File")
-	def show_fcf(self):
+		else:
+			msgBox = QMessageBox()
+			msgBox.setText("File does not exist, please enter another name")
+			msgBox.setWindowTitle("File")
+			msgBox.exec_()
+	def read_fcf(self,f):
+		#read an fcf file
+		return any_reflection_file(str(f))
+	def show_fcf(f):
 		pass
 		
 	#Operations about CIF file
 	def load_cif(self):
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
-		self.cif_name, _ = QFileDialog.getOpenFileName(self,"Choose a CIF file", "","All Files (*);;CIF Files (*.cif)", options=options)
+		self.cif_name, _ = QFileDialog.getOpenFileName(self,"Choose a CIF file", "","CIF Files (*.cif);;All Files (*)", options=options)
 		self.textbox4.setText(self.cif_name)
 	def cif_text_editor_enter(self):
 		tmp = self.textbox4.text()
@@ -97,6 +144,23 @@ class Main_Window(QMainWindow):
 			msgBox = QMessageBox()
 			msgBox.setText("Successfully found CIF file.")
 			msgBox.setWindowTitle("CIF File")
+		else:
+			msgBox = QMessageBox()
+			msgBox.setText("File does not exist, please enter another name")
+			msgBox.setWindowTitle("File")
+			msgBox.exec_()
+	def read_cif(self,f):
+		#read a cif file  One cif file can contain multiple structures
+		try:
+			if isinstance(f,str):
+				structures=cif.reader(file_path=f,raise_if_errors=True).build_crystal_structures()
+			else:
+				raise TypeError,'read_cif: Cannot deal is type {}'.format(type(f))
+		except libtbx.utils.Sorry as e:
+			print e
+			print "Error parsing cif file, check if the data tag does not contain any spaces."
+			exit()
+		return structures
 	def show_cif(self):
 		pass
 		
@@ -129,17 +193,91 @@ class Main_Window(QMainWindow):
 			msgBox.setWindowTitle("Error")
 			msgBox.exec_()
 	
-	def save_fobs_fcalc():
+	def save_fobs_fcalc(self):
 		try:
 			options = QFileDialog.Options()
 			options |= QFileDialog.DontUseNativeDialog
-			self.cif_name, _ = QFileDialog.getSaveFileName(self,"Save Fobs Fcalc", "","JSON Files (*.json);;All Files (*)", options=options)
+			save_name, _ = QFileDialog.getSaveFileName(self,"Save Fobs Fcalc", "","JSON Files (*.json);;All Files (*)", 										options=options)
+			with open(save_name,'w') as fp:
+				json.dump(self.saved_Data,fp,indent=4)
 		except:
 			msgBox = QMessageBox()
 			msgBox.setIcon(QMessageBox.Critical)
-			msgBox.setText("An Error Has Ocurred while trying to save fobs and fcalc! Please check if you input the proper number.")
+			msgBox.setText("An Error Has Ocurred while trying to save fobs and fcalc!")
 			msgBox.setWindowTitle("Error")
 			msgBox.exec_()
+			
+	def f_calc_structure_factors(self,structure,**kwargs):
+		"""Takes cctbx structure and returns f_calc miller array
+		Takes an optional options dictionary with keys:
+		input:
+			**kwargs:
+				'd_min': minimum d-spacing for structure factor calculation
+				'algorithm': which algorithm to use ('direct', 'fft', 'automatic')
+			structure: <cctbx.xray.structure.structure object>
+		output:
+			f_calc: <cctbx.miller.array object> with calculated structure factors
+				in the f_calc.data() function
+		
+		"""
+	 
+		dmin        = kwargs.get('dmin',1.0)
+		algorithm 	= kwargs.get('algorithm',"automatic")
+		anomalous 	= kwargs.get('anomalous',False)
+		table 		= kwargs.get('scatfact_table','wk1995')
+		return_as   = kwargs.get('return_as',"series")
+		verbose     = kwargs.get('verbose', False)
+
+		if dmin <= 0.0:
+			raise ValueError, "d-spacing must be greater than zero."
+
+		if algorithm == "automatic":
+			if structure.scatterers().size() <= 100:
+				algorithm = "direct"
+			else:
+				algorithm = None
+
+		structure.scattering_type_registry(table=table)
+
+		f_calc_manager = structure.structure_factors(
+				anomalous_flag = anomalous,
+				d_min = dmin,
+				algorithm = algorithm)
+		f_calc = f_calc_manager.f_calc()
+		
+		if verbose:
+			print "\nScattering table:", structure.scattering_type_registry_params.table
+			structure.scattering_type_registry().show()
+		print "Minimum d-spacing: %g" % f_calc.d_min()
+
+		if return_as == "miller":
+			return f_calc
+		elif return_as == "series":
+			fcalc = pd.Series(index=f_calc.indices(),data=np.abs(f_calc.data()))
+			phase = pd.Series(index=f_calc.indices(),data=np.angle(f_calc.data()))
+			return fcalc,phase
+		elif return_as == "df":
+			dffcal = pd.DataFrame(index=f_calc.index)
+			dffcal['fcalc'] = np.abs(f_calc.data())
+			dffcal['phase'] = np.angle(f_calc.data())
+			return dffcal
+		else:
+			raise ValueError, "Unknown argument for 'return_as':{}".format(return_as)
+			
+	def calc_structure_factors(self,structures,dmin=1.0,table='electron',prefix='',verbose=True,**kwargs):
+		"""Wrapper around f_calc_structure_factors()
+		Takes a structure object in which there is only one strcture
+
+		dmin can be a dataframe and it will take the minimum dspacing (as specified by col 'd') or a float
+		if combine is specified, function will return a dataframe combined with the given one, otherwise a
+		dictionary of dataframes
+
+		prefix is a prefix for the default names fcalc/phases to identify different structures
+		"""    
+		fcalc = self.f_calc_structure_factors(structures,dmin=dmin,scatfact_table=table,\
+										return_as="miller",verbose=verbose,**kwargs)
+
+		return fcalc
 	
 	def home(self):
 		#quit button
@@ -306,12 +444,12 @@ class Main_Window(QMainWindow):
 	def plot_Fcalc_Fobs(self):
 		try:
 			if self.cb1_1.checkState():#1 get Fobs from mtz file
-				mtz_file = read_mtz(self.textbox1.text())
+				mtz_file = self.read_mtz(self.textbox1.text())
 				Fobs = mtz_file.as_miller_arrays()[2]
 				Fobs_DF = pd.DataFrame(index=pd.MultiIndex.from_tuples(Fobs.indices()),\
 									data=np.array(Fobs.data()*self.scaling_factor))
 			elif self.cb2_1.checkState():#2 get Fobs from fcf file
-				fcf_file = read_fcf(self.textbox3.text())
+				fcf_file = self.read_fcf(self.textbox3.text())
 				model = dict(fcf_file.file_content().model().items()[0][1])
 				if model['_shelx_refln_list_code'] is '6':
 					Fobs = fcf_file.as_miller_arrays()[1] #This Fobs is Fobs^2 but in order to be more convinient for me just call it Fobs
@@ -329,17 +467,17 @@ class Main_Window(QMainWindow):
 		#-------------------------------------------------------------------------------
 			if self.cb1.checkState():#1 get Fcalc by calculating the structure factor from pdb file
 				self.scatfact_table = str(self.combo.currentData())
-				pdb_structure = read_pdb(self.textbox2.text())
-				Fcalc = calc_structure_factors(pdb_structure,dmin=self.resolution,table=self.scatfact_table)
+				pdb_structure = self.read_pdb(str(self.textbox2.text()))
+				Fcalc = self.calc_structure_factors(pdb_structure,dmin=self.resolution,table=self.scatfact_table)
 				Fcalc_data = Fcalc.data()
 				Fcalc_indices = Fcalc.indices()
 				Fcalc_DF = pd.DataFrame(index=pd.MultiIndex.from_tuples(Fcalc_indices),data=np.abs(Fcalc_data))
 				
 			elif self.cb2.checkState():#2 get Fcalc by calculating the structure factor from cif file
 				self.scatfact_table = str(self.combo.currentData())
-				cif_structures = read_cif(str(self.textbox4.text()))
+				cif_structures = self.read_cif(str(self.textbox4.text()))
 				for name, cif_structure in cif_structures.items():
-					Fcalc = calc_structure_factors(cif_structure,dmin=self.resolution,table=self.scatfact_table)
+					Fcalc = self.calc_structure_factors(cif_structure,dmin=self.resolution,table=self.scatfact_table)
 					break #abandon any more structures in the cif file, if there is any, only read the first one
 				Fcalc_P1 = Fcalc.expand_to_p1()
 				Fcalc_data = Fcalc_P1.data()
@@ -348,13 +486,13 @@ class Main_Window(QMainWindow):
 				
 			elif self.cb3.checkState():#3 get Fcalc from mtz file
 				if not self.cb1_1.checkState():
-					mtz_file = read_mtz(self.textbox1.text())
+					mtz_file = self.read_mtz(self.textbox1.text())
 				Fcalc = mtz_file.as_miller_arrays()[3]
 				Fcalc_DF = pd.DataFrame(index=pd.MultiIndex.from_tuples(Fcalc.indices()),data=np.abs(Fcalc.data()))
 
 			elif self.cb4.checkState():#4 get Fcalc from fcf file
 				if not self.cb2_1.checkState():
-					fcf_file = read_fcf(self.textbox3.text())
+					fcf_file = self.read_fcf(self.textbox3.text())
 					model = dict(fcf_file.file_content().model().items()[0][1])
 					
 				if model['_shelx_refln_list_code'] is '6':
@@ -376,9 +514,10 @@ class Main_Window(QMainWindow):
 			for i in Fobs.indices():
 				merged_x.append(Fobs_DF.loc[i][0])
 				merged_y.append(Fcalc_DF.loc[i][0])
-				
 			plt.figure()
 			x2, y2 = pd.Series(merged_x, name="F_obs"), pd.Series(merged_y, name="F_model")
+			self.saved_Data['Fobs'] = merged_x
+			self.saved_Data['Fcalc'] = merged_y
 			ax = sns.regplot(x=x2, y=y2, marker='+')
 			plt.show()
 			
@@ -391,112 +530,36 @@ class Main_Window(QMainWindow):
 			msgBox.setWindowTitle("Error")
 			msgBox.exec_()
 			
-def read_pdb(f):
-    #read a pdb file
-    try:
-        if isinstance(f,str):
-            structures=pdb.input(file_name=f,raise_sorry_if_format_error=True).xray_structure_simple()
-        else:
-            raise TypeError,'read_pdb: Cannot deal is type {}'.format(type(f))
-    except libtbx.utils.Sorry as e:
-        print e
-        print "Error parsing pdb file, check if the data tag does not contain any spaces."
-        exit()
-    return structures
-	
-def read_cif(f):
-	#read a cif file  One cif file can contain multiple structures
-	try:
-		if isinstance(f,str):
-			structures=cif.reader(file_path=f,raise_if_errors=True).build_crystal_structures()
+class File_Window(QMainWindow):
+	def __init__(self,f):
+		super(File_Window,self).__init__()
+		self.initUI()
+		self.show_file(f)
+		self.show()
+		
+	def initUI(self):
+		self.setGeometry(400, 400, 600, 800)
+		self.setFixedSize(self.size())
+		self.setWindowIcon(QIcon('gear-tools.png'))  
+		self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
+		
+	def show_file(self,f):
+		appendix = f.split('.')[-1].lower()
+		print appendix, f
+		if appendix == 'mtz':
+			print f
+		elif appendix == 'pdb': 
+			pass
+		elif appendix == 'fcf':
+			pass
+		elif appendix == 'cif':
+			pass
 		else:
-			raise TypeError,'read_cif: Cannot deal is type {}'.format(type(f))
-	except libtbx.utils.Sorry as e:
-		print e
-		print "Error parsing cif file, check if the data tag does not contain any spaces."
-		exit()
-	return structures
-	
-def read_mtz(f):
-	#read an mtz file
-	return any_reflection_file(str(f))
-	
-def read_fcf(f):
-	#read an fcf file
-	return any_reflection_file(str(f))
-
-def f_calc_structure_factors(structure,**kwargs):
-    """Takes cctbx structure and returns f_calc miller array
-	Takes an optional options dictionary with keys:
-	input:
-		**kwargs:
-			'd_min': minimum d-spacing for structure factor calculation
-			'algorithm': which algorithm to use ('direct', 'fft', 'automatic')
-		structure: <cctbx.xray.structure.structure object>
-	output:
-		f_calc: <cctbx.miller.array object> with calculated structure factors
-			in the f_calc.data() function
-	
-    """
- 
-    dmin        = kwargs.get('dmin',1.0)
-    algorithm 	= kwargs.get('algorithm',"automatic")
-    anomalous 	= kwargs.get('anomalous',False)
-    table 		= kwargs.get('scatfact_table','wk1995')
-    return_as   = kwargs.get('return_as',"series")
-    verbose     = kwargs.get('verbose', False)
-
-    if dmin <= 0.0:
-        raise ValueError, "d-spacing must be greater than zero."
-
-    if algorithm == "automatic":
-        if structure.scatterers().size() <= 100:
-            algorithm = "direct"
-        else:
-            algorithm = None
-
-    structure.scattering_type_registry(table=table)
-
-    f_calc_manager = structure.structure_factors(
-            anomalous_flag = anomalous,
-            d_min = dmin,
-            algorithm = algorithm)
-    f_calc = f_calc_manager.f_calc()
-	
-    if verbose:
-        print "\nScattering table:", structure.scattering_type_registry_params.table
-        structure.scattering_type_registry().show()
-    print "Minimum d-spacing: %g" % f_calc.d_min()
-
-    if return_as == "miller":
-        return f_calc
-    elif return_as == "series":
-        fcalc = pd.Series(index=f_calc.indices(),data=np.abs(f_calc.data()))
-        phase = pd.Series(index=f_calc.indices(),data=np.angle(f_calc.data()))
-        return fcalc,phase
-    elif return_as == "df":
-        dffcal = pd.DataFrame(index=f_calc.index)
-        dffcal['fcalc'] = np.abs(f_calc.data())
-        dffcal['phase'] = np.angle(f_calc.data())
-        return dffcal
-    else:
-        raise ValueError, "Unknown argument for 'return_as':{}".format(return_as)
-        
-def calc_structure_factors(structures,dmin=1.0,table='electron',prefix='',verbose=True,**kwargs):
-    """Wrapper around f_calc_structure_factors()
-    Takes a structure object in which there is only one strcture
-
-    dmin can be a dataframe and it will take the minimum dspacing (as specified by col 'd') or a float
-    if combine is specified, function will return a dataframe combined with the given one, otherwise a
-    dictionary of dataframes
-
-    prefix is a prefix for the default names fcalc/phases to identify different structures
-    """    
-    fcalc = f_calc_structure_factors(structures,dmin=dmin,scatfact_table=table,\
-                                    return_as="miller",verbose=verbose,**kwargs)
-
-    return fcalc
-	
+			msgBox = QMessageBox()
+			msgBox.setIcon(QMessageBox.Critical)
+			msgBox.setText("An Error Has Ocurred while trying to show the {} file!".format(appendix.upper()))
+			msgBox.setWindowTitle("Error")
+			msgBox.exec_()
 		
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
